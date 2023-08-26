878472be8d231376f964826cddbc3d64dd12e86d
Davina Wijaya <phandavina@gmail.com>
2023-08-27 02:16:32 +0800

    added debugging lines for CUD operations

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -19,6 +19,7 @@ const firestoreReducer = (state, action) => {
     case 'ERROR':
       return { isPending: false, document: null, success: false, error: action.payload }
     case "UPDATED_DOCUMENT":
+      console.log('Payload for UPDATED_DOCUMENT: ', action.payload);
       return { isPending: false, document: action.payload, success: true,  error: null }
     default:
       return state
@@ -41,42 +42,63 @@ export const useFirestore = (collection) => {
   
   // add a document
   const addDocument = async (doc) => {
+    console.log("addDocument called");
     dispatch({ type: 'IS_PENDING' })
+    console.log("Dispatched IS_PENDING");
 
     try {
       const createdAt = timestamp.fromDate(new Date())
       const addedDocument = await ref.add({ ...doc, createdAt })
+      console.log("Document added", addedDocument);
       dispatchIfNotCancelled({ type: 'ADDED_DOCUMENT', payload: addedDocument })
+      console.log("Dispatched ADDED_DOCUMENT");
     }
     catch (err) {
+      console.log("Error in addDocument", err);
       dispatchIfNotCancelled({ type: 'ERROR', payload: err.message })
+      console.log("Dispatched ERROR");
     }
   }
 
   // delete a document
   const deleteDocument = async (id) => {
+    console.log("deleteDocument called");
     dispatch({ type: 'IS_PENDING' })
+    console.log("Dispatched IS_PENDING");
 
     try {
       await ref.doc(id).delete()
+      console.log("Document deleted");
       dispatchIfNotCancelled({ type: 'DELETED_DOCUMENT' })
+      console.log("Dispatched DELETED_DOCUMENT");
     }
     catch (err) {
+      console.log("Error in deleteDocument", err);
       dispatchIfNotCancelled({ type: 'ERROR', payload: 'could not delete' })
+      console.log("Dispatched ERROR");
     }
   }
 
+  
   // update a document
   const updateDocument = async (id, updates) => {
+    console.log("updateDocument called");
+    console.log("Document ID to be updated: ", id);
+    console.log("Updates to be made: ", updates);
     dispatch({ type: "IS_PENDING" })
+    console.log("Dispatched IS_PENDING");
 
     try {
       const updatedDocument = await ref.doc(id).update(updates)
+      console.log("Document updated", updatedDocument);
       dispatchIfNotCancelled({ type: "UPDATED_DOCUMENT", payload: updatedDocument })
+      console.log("Dispatched UPDATED_DOCUMENT");
       return updatedDocument
     } 
     catch (error) {
+      console.log("Error in updateDocument", error);
       dispatchIfNotCancelled({ type: "ERROR", payload: error })
+      console.log("Dispatched ERROR");
       return null
     }
   }
